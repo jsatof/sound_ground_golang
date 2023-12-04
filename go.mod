404080fd523d56775957d014e3b67a125b52991f
James Ferrarelli <jsferr4@gmail.com>
2023-12-04 17:47:45 -0500

    WIP: configuration for wav parsing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gitlab.com/jsfer/sound_ground_golang
 
 go 1.18
+
+require github.com/gordonklaus/portaudio v0.0.0-20230709114228-aafa478834f5 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,53 @@
 package main
 
 import (
+	//"io"
+	"os"
 	"fmt"
+
+	"github.com/gordonklaus/portaudio"
 )
 
+const (
+	SampleFormatUnsupported int = 0
+	SampleFormatF64 = 1
+)
+
+const (
+	CodecUnsupported int = 20
+	CodecWAV = 21
+)
+
+type AudioProps struct {
+	Channels int
+	SampleRate int
+	Format int
+	Codec int
+}
+
+// looking for the 'RIFF' header, and populate properties
+func validateWAV(file *os.File) (props AudioProps, err error) {
+	buffer := make([]byte, 4)
+	//found, err = io.ReadAt(buffer, 0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if string(buffer) == "RIFF" {
+		props.Codec = CodecWAV
+	}
+	return
+}
+
 func main() {
-	fmt.Println(HelloWorld())
+	wav, err := os.Open("Periphery_Wildfire_rhythm_guitar_stem.wav")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer wav.Close()
+
+	portaudio.Initialize()
+	defer portaudio.Terminate()
 }
